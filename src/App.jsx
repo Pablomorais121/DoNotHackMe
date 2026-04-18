349060b66571e8d461e4fb979052979a0fbeaa54
Pablomorais121 <pablomoraisalvarez@gmail.com>
2026-04-18 16:08:17 +0200

    chore:: basic project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,23 @@
+import {useState} from 'react'
+
+const initialState = {
+  turn: 1,
+  player: {
+    hp: 100,
+    budget: 0,
+    actionPoints: 2,
+    redTeam: {level: 1, actions: ['recon']},
+    blueTeam: {level: 1, actions: ['monitor']},
+  },
+  rival: {
+    hp: 100,
+  },
+  log: []
+}
+
 function App() {
+  const [gameState, setGameState] = useState(initialState)
+
   return <div>DoNotHackMe</div>
 }
 
